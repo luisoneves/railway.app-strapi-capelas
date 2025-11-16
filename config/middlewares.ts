074c96b54494417fbfffbc8707a94ecbf3472457
luisoneves <1991lotavion@gmail.com>
2025-11-15 23:17:50 -0300

    fix: update CORS settings

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,20 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000',
+        'https://capela-site-frontend.vercel.app',
+        'https://capela-site-frontend-8cbx89fk4-1991lotavio-1655s-projects.vercel.app',
+      ],
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
